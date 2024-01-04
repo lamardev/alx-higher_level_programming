0ffe19e5390186a78d9372e4e66b178c2e6f5b77
lamardev <babashehushettima@outlook.com>
2024-01-04 01:25:29 +0100

    Python script that prints the last digit of a number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -15,6 +15,6 @@ print(f"Last digit of {number} is {last_digit} and", end=" ")
 if last_digit > 5:
     print(f"is greater than 5")
 elif last_digit == 0:
-    print(f"is zero")
+    print(f"is 0")
 else:
     print(f"is less than 6 and not 0")
