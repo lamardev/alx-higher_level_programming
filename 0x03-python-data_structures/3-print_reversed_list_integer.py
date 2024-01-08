6c8a79678ec815e17ba5b5187de8b314ecdce398
lamardev <babashehushettima@outlook.com>
2024-01-09 00:07:57 +0100

    Fixing string format

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
-    return [print("{}".format(i)) for i in my_list]
+    return [print("{:d}".format(i)) for i in my_list]
