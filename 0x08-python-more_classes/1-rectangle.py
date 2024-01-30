a34c67a5653d568aed5740d1935891446be1c55a
lamardev <babashehushettima@outlook.com>
2024-01-30 02:11:01 +0100

    Creating a class Rectangle with property setters and getters

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,12 +12,12 @@ class Rectangle:
         """ Initializes a new `Rectangle` """
         self.width = width
         self.height = height
-    
+
     @property
     def width(self):
         """ Gets the width value """
         return self.__width
-    
+
     @property
     def height(self):
         """ Gets the height value """
@@ -26,16 +26,16 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """ Sets the value of width """
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-    
+
     @height.setter
     def height(self, value):
         """ Sets the value of height """
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
